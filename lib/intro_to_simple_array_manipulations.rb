bbb7157131481e833175b03d8c166d3e85f4b070
andreancardona@gmail.com <andreancardona@gmail.com>
2017-11-22 13:00:09 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -26,4 +26,9 @@ end
 
 def using_concat (array1, array2)
   array1.concat(array2)
+      #def using_concat(my_favorite_things, more_favs)
+      #@my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
+      #more_favs = ["mario kart", "flatiron school"]
+      #@all_my_favs = @my_favorite_things.concat(more_favs)
+      #end
 end
