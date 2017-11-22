3e28dad8a2e8fb2c3ab1157f5a43543c15380e33
andreancardona@gmail.com <andreancardona@gmail.com>
2017-11-22 12:37:56 +0000

    automatically backed up by learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,9 @@
+def using_push (array, string)
+      array.push(string)
+    end
+def using_unshift (array, string)
+    array.unshift(string)
+    #def using_unshift(neighborhoods_in_northwest_brooklyn, new_neighborhood)
+   #neighborhoods_in_northwest_brooklyn = ["Brooklyn Yards", "Cadman Plaza", "Clinton Hill", "Downtown Brooklyn", "DUMBO"]
+  #@new_neighborhood = "Brooklyn Heights"
+end
