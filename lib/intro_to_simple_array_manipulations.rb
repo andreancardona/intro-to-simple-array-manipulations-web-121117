9df376b4c546b2ffc9e2b9d9e5649b4ce8070c92
andreancardona@gmail.com <andreancardona@gmail.com>
2017-11-22 13:21:06 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -46,11 +46,12 @@ def using_flatten(array)
 end
 
 def using_delete (array, string)
-  array.delete(string)
+  no_offense_steven = array.delete(string)
 end
 
 
 def using_delete_at (array, integer)
+   
    array.delete(integer)
 end
 
